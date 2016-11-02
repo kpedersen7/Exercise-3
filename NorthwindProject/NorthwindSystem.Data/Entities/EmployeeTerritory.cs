bb689cb4fd0b9504fc1cc2dec53a3cdf193ef958
Kyle Pedersen <kpedersen7@studentmail.nait.ca>
2016-11-02 01:46:06 -0600

    Created Entities
    Employees, Territories, EmployeeTerritories, and Regions

---
+++ NorthwindProject/NorthwindSystem.Data/Entities/EmployeeTerritory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace NorthwindSystem.Data.Entities
+{
+    [Table("EmployeeTerritories")]
+    public class EmployeeTerritory
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        public int TerritoryID { get; set; }
+
+        public virtual Employee Employee { get; set; }
+        public virtual Territory Territory { get; set; }
+
+    }
+}
